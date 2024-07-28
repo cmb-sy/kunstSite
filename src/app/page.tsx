10eba5487f06a7d078ff2690cad49d776bbf253f
cmb-sy <free.nakashima@gmail.com>
2024-07-29 08:31:20 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from "next";
+import Head from "next/head";
 
 export const metadata: Metadata = {
-  title: "Nextjs超初心者入門",
+  title: "Nextjs超初心者入門だよ",
   description: "SSR,SSG,ISRでブログを作成する。",
   openGraph: {
     title: "Nextjs超初心者入門",
@@ -21,7 +22,16 @@ export const metadata: Metadata = {
 };
 
 const Home = () => {
-  return <div className="text-[50px]">Nextjs超初心者入門</div>;
+  return (
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+      </Head>
+      <div className="text-[50px]">Nextjs超初心者入門</div>
+    </>
+  );
 };
 
 export default Home;
