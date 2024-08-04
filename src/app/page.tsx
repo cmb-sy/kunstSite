364943af5d64395e72ddfc89e3df5f3ef902748c
cmb-sy <free.nakashima@gmail.com>
2024-08-04 20:20:08 +0900

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Container from "@/app/components/container";
+import { Intro } from "@/app/components/intro";
+import { MoreStories } from "@/app/components/more-stories";
+import { getAllPosts } from "@/lib/api";
+
+export default function Index() {
+  const allPosts = getAllPosts();
+
+  const heroPost = allPosts[0];
+
+  const morePosts = allPosts.slice(1);
+
+  return (
+    <main>
+      <Container>
+        <Intro />
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Container>
+    </main>
+  );
+}
