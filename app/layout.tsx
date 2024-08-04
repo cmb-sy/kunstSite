f1bb0a8470578eda4c667805a667822da6b3f847
cmb-sy <free.nakashima@gmail.com>
2024-08-04 19:56:28 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Noto_Sans_JP } from "next/font/google";
-
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
-const noto = Noto_Sans_JP({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,11 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={noto.className}>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
