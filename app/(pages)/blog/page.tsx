7212524fbeb2e66ef80941b34a434c48cf9af7a3
cmb-sy <free.nakashima@gmail.com>
2024-08-15 10:16:54 +0900

    update

--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -1,7 +1,7 @@
 import { marked } from "marked";
 import Link from "next/link";
 
-const stripMarkdownAndHtml = async (markdown: string): Promise<string> => {
+const stripMarkdown = async (markdown: string): Promise<string> => {
   const html = await marked(markdown);
   const text = html.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "");
   return text.trim();
@@ -20,7 +20,7 @@ const BlogPage = async () => {
         {await Promise.all(
           blogData.map(async (post: any, index: number) => {
             const strippedContent = (
-              await stripMarkdownAndHtml(post.content)
+              await stripMarkdown(post.content)
             ).substring(0, 70);
             return (
               <div key={index} className="p-4 border rounded shadow-sm">
@@ -39,38 +39,3 @@ const BlogPage = async () => {
 };
 
 export default BlogPage;
-
-// import Link from "next/link";
-// import { Post } from "@/types/post";
-
-// const stripHtml = (html: string): string => {
-//   const htmlText = html.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "");
-//   return htmlText;
-// };
-
-// const getBlogData = async () => {
-//   const res = await fetch("http://localhost:3000/api/blog");
-//   const blogData = await res.json();
-//   return blogData;
-// };
-
-// const Home = async () => {
-//   const blogData = await getBlogData();
-//   return (
-//     <div className="container mx-auto py-[50px]">
-//       <div className="grid grid-cols-2 gap-6">
-//         {blogData.map((blog: Post) => (
-//           <Link href={`/blog/${blog.slug}`} className="w-full" key={blog.slug}>
-//             <div className="border border-black rounded p-5">
-//               <h2 className="text-xl font-bold">{blog.title}</h2>
-//               <p className="text-gray-500">{blog.date}</p>
-//               <p>{stripHtml(blog.blogContentsHTML).substring(0, 60)}...</p>
-//             </div>
-//           </Link>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
--- utils/getPostsData.ts
+++ utils/getPostsData.ts
@@ -5,31 +5,11 @@ import matter from "gray-matter";
 
 const postsDirectoryPath = path.join(process.cwd(), "posts");
 
-// export async function getPostBySlug(slug: string) {
-//   const realSlug = slug.replace(/\.mdx$/, "");
-//   const fullPath = path.join(postsDirectoryPath, `${realSlug}.mdx`);
-//   const fileContents = fs.readFileSync(fullPath, "utf8");
-
-//   // MDX をコンパイル
-//   const { content, frontmatter } = await compileMDX<{ title: string }>({
-//     source: fileContents,
-//     // components: { Highlight },
-//     // options: { parseFrontmatter: true },
-//   });
-//   console.log(content);
-//   return {
-//     ...frontmatter,
-//     content,
-//     slug: realSlug,
-//   };
-// }
-
 export async function getPostBySlug(slug: string) {
   const realSlug = slug.replace(/\.mdx$/, "");
   const fullPath = path.join(postsDirectoryPath, `${realSlug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const matterResult = matter(fileContents);
-  // compileMDXでまとめて可能だが、一覧と記事で分けるため使用しない。
   return {
     ...matterResult.data,
     content: matterResult.content,
@@ -42,28 +22,3 @@ export async function getAllPosts() {
   const posts = await Promise.all(slugs.map((slug) => getPostBySlug(slug)));
   return posts;
 }
-
-// import fs from "fs";
-// import matter from "gray-matter";
-// import path from "path";
-// import html from "remark-html";
-// import { remark } from "remark";
-// import { Post } from "@/types/post";
-
-// const postsDirectoryPath = path.join(process.cwd(), "posts");
-
-// export async function getPostBySlug(slug: string) {
-//   const realSlug = slug.replace(/\.md$/, "");
-//   const fullPath = path.join(postsDirectoryPath, `${realSlug}.md`);
-//   const fileContents = fs.readFileSync(fullPath, "utf8");
-//   const matterResult = matter(fileContents);
-//   const blogContents = await remark().use(html).process(matterResult.content);
-//   const blogContentsHTML = blogContents.toString();
-//   return { ...matterResult.data, slug: realSlug, blogContentsHTML } as Post;
-// }
-
-// export async function getAllPosts(): Promise<Post[]> {
-//   const slugs = fs.readdirSync(postsDirectoryPath);
-//   const posts = await Promise.all(slugs.map((slug) => getPostBySlug(slug)));
-//   return posts;
-// }
