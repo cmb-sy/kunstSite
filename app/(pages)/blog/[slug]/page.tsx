8f6bc089993ef3eb1f7d286520cd4cff27df325a
cmb-sy <free.nakashima@gmail.com>
2024-08-16 11:40:49 +0900

    update

--- app/(pages)/blog/[slug]/page.tsx
+++ app/(pages)/blog/[slug]/page.tsx
@@ -6,10 +6,9 @@ import ProfileCard from "@/app/components/ProfileCard";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import rehypePrism from "rehype-prism";
-// rehype-prismでpythonのシンタックス・ハイライトをするために必要
 import "prismjs/components/prism-python.js";
-// シンタックス・ハイライト用のCSS
 import "prismjs/themes/prism-tomorrow.css";
+
 // SSG
 export async function generateStaticParams() {
   const res = await fetch("http://localhost:3000/api/blog/", {
@@ -33,16 +32,16 @@ const BlogArticlePage = async ({ params }: { params: { slug: string } }) => {
   const blogArticle = await getBlogArticle(params.slug);
 
   return (
-    <div className="container mx-auto py-5 px-2 lg:px-10 flex justify-center w-full">
-      <div className="flex flex-col lg:flex-row w-full max-w-5xl">
-        <div className="w-full lg:w-3/4 bg-white bg-gray-100 p-5 rounded-lg min-h-screen">
+    <div className="container mx-auto py-5 px-2 lg:px-10 flex justify-end w-full">
+      <div className="flex flex-col w-full max-w-5xl">
+        <div className="w-full lg:w-4/5 bg-white bg-gray-100 p-5 rounded-lg min-h-screen">
           <h1 className="text-3xl font-bold text-gray-800">
             {blogArticle.title}
           </h1>
           <br />
           <div className="text-gray-600">{blogArticle.date}</div>
           <br />
-          <div className="prose prose-lg text-gray-700">
+          <div className="prose prose-lg text-gray-700  max-w-4xl">
             <link
               rel="stylesheet"
               href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
@@ -62,7 +61,7 @@ const BlogArticlePage = async ({ params }: { params: { slug: string } }) => {
           </div>
         </div>
       </div>
-      <aside className="w-full lg:w-1/4 bg-gray-200 p-5 rounded-lg mt-5 lg:mt-0 lg:ml-20 hidden lg:block">
+      <aside className="w-full lg:w-1/3 bg-gray-200 p-5 rounded-lg mt-5 lg:mt-0 lg:ml-20 hidden lg:block">
         <ProfileCard />
         <div className="text-gray-700 mt-5">サイドバーのコンテンツ</div>
       </aside>
