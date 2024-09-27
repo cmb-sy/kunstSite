333c0961e66e0587395fd758358e4137a1dfaadd
cmb-sy <free.nakashima@gmail.com>
2024-09-27 09:54:37 +0900

    余分なカンマを削除

--- app/components/ArticleLists.tsx
+++ app/components/ArticleLists.tsx
@@ -30,7 +30,7 @@ const ArticleLists: React.FC<ArticleListsProps> = async ({
       <section className="container mx-auto section-style3">
         <div className="flex justify-center items-center p-4">
           <button className="text-lg font-medium px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
-            {categoryLabel},
+            {categoryLabel}
           </button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
