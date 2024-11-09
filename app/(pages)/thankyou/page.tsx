a1d0dba4b02c3b31750c6b93fc4c6c38bd08adfc
cmb-sy <free.nakashima@gmail.com>
2024-11-10 00:06:42 +0900

    update

--- app/(pages)/thankyou/page.tsx
+++ app/(pages)/thankyou/page.tsx
@@ -1,18 +1,28 @@
 import { HiOutlineMailOpen } from "react-icons/hi";
+import Link from "next/link";
+
 const ThankYou = () => {
   return (
-    <section className="thank-you">
+    <section className="relative min-h-screen mt-32">
       <div className="mx-auto">
         <div className="mt-8 flex justify-center">
-          <HiOutlineMailOpen className="text-6xl text-gray-500" />
+          <HiOutlineMailOpen className="text-8xl text-gray-300" />
         </div>
-        <h2 className="my-10 text-center">お問合せありがとうございます</h2>
+        <div className="my-10 text-center text-2xl md:text-4xl font-bold">
+          お問合せありがとうございます
+        </div>
         <p className="mt-4 text-center">
           お問合せ内容を確認させていただきますので、
           <br />
           しばらくお待ちください。
         </p>
-        <div className="flex justify-center">ボタン</div>
+        <div className="flex justify-center mt-10">
+          <Link href="/" passHref>
+            <button className="bg-gray-800 hover:bg-gray-700 text-white rounded px-4 py-2">
+              トップページへ戻る
+            </button>
+          </Link>
+        </div>
       </div>
     </section>
   );
