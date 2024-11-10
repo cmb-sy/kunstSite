8d31ba8ec4035fcd094f2409afa0adec26cce2f9
cmb-sy <free.nakashima@gmail.com>
2024-11-10 09:21:45 +0900

    contactの修正

--- app/components/features/HomePage/Contact.tsx
+++ app/components/features/HomePage/Contact.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 
 const Contact = () => {
+  const apiUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://www.cmb-sy.jp/contact"
+      : "http://localhost:3000/contact";
   return (
     <div className="flex flex-col items-center justify-center w-screen p-20">
       <h1 className="sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center text-black-500">
@@ -14,7 +18,7 @@ const Contact = () => {
         <br />
         下記よりお問い合わせください。
       </span>
-      <Link href="http://localhost:3000/contact">
+      <Link href={apiUrl}>
         <button className="px-20 py-3 bg-black text-white rounded-lg shadow-md transform transition-transform duration-200 hover:scale-105 hover:shadow-lg">
           contact me
         </button>
